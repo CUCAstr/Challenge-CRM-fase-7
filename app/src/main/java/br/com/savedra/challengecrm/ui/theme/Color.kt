175d396628c05cdc1fa46b8890acf61a11fdb35c
Juan Savedra <juansavedra827@gmail.com>
2025-10-15 16:10:38 -0300

    style: Colors

--- app/src/main/java/br/com/savedra/challengecrm/ui/theme/Color.kt
+++ app/src/main/java/br/com/savedra/challengecrm/ui/theme/Color.kt
@@ -2,10 +2,26 @@ package br.com.savedra.challengecrm.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+/* Cores primárias */
+val bgIndigo500 = Color(0xFF6366F1)
+val bgIndigo600 = Color(0xFF4F46E5)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+/* Cores neutras (escala de cinza - slate) */
+val slate50 = Color(0xFFF8FAFC)
+val slate100 = Color(0xFFF1F5F9)
+val slate200 = Color(0xFFE2E8F0)
+val slate400 = Color(0xFF94A3B8)
+val slate500 = Color(0xFF64748B)
+val slate600 = Color(0xFF475569)
+val slate700 = Color(0xFF334155)
+val slate800 = Color(0xFF1E293B)
+
+/* Cores de status e acentos */
+val green500 = Color(0xFF22C55E)
+val blue100 = Color(0xFFDBEAFE)
+val purple100 = Color(0xFFF3E8FF)
+val purple500 = Color(0xFFA855F7)
+
+/* Básicas */
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
